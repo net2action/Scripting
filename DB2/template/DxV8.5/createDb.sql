e1ea797e68ec860335ee3138f9b4f3e9298b54d7
Andrea Fontana <a.fontana@net2action.com>
2017-01-26 17:15:55 +0100

    Add files via upload

--- DB2/template/DxV8.5/createDb.sql
+++ DB2/template/DxV8.5/createDb.sql
@@ -7,8 +7,33 @@
 #GRANT DBADM, SECADM ON DATABASE TO USER ${db2Admin};
 #CONNECT RESET;
 
+PROGNAME=$(basename $0)
+AUTHOR="A.Fontana"
+VERSION="1.0.0"
+OWNER=" by net2action - 2016"
+
+
+function error_exit
+{
+        echo "${PROGNAME}:[$LINENO] ${1}"
+        echo "[$now] ${PROGNAME}:[$LINENO] ${1}" >> ${logFile}
+        msgLine "---------------------------------------"
+        echo "BUILD FAIL"
+        if [ "$DEBUG" == "True" ] ; then
+	    msgLine " DEBUG ------>  Script: $script db: ${DBName} USER ${db2Admin} USING ${db2Pwd}" 1>&2
+	fi
+        exit ${rc}
+}
+
+function msgLine
+{
+        echo "${PROGNAME}:[$LINENO] ${1}" 1>&2
+        echo "[$now] ${PROGNAME}:[$LINENO] ${1}" >> ${logFile}
+}
+
+
 if [ "$DEBUG" == "True" ] ; then
-   echo " db: " ${DBName} "--"  ${db2Admin}
+   msgLine " DEBUG ------>  db:  ${DBName} --  ${db2Admin}"
 fi
 
 db2 -t -x -z sql.out +p <<-eof
@@ -16,3 +41,15 @@ CREATE DB ${DBName} ON ${dataMountPoint} using codeset UTF-8 territory us PAGESI
 UPDATE DB CFG FOR ${DBName} USING locktimeout 30;
 terminate;
 eof
+
+rc=$?
+if [ ${rc} -ne  0 ] ; then
+        msgLine "Stream sql command fail:  ${rc}"
+        error_exit "$LINENO: Stream sql command [ $script ] fail: ${rc}"
+fi
+
+msgLine "---------------------------------------"
+msgLine "$PROGNAME    BUILD SUCCESS"
+msgLine "End: $(date)"
+
+exit 0
--- DB2/template/DxV8.5/extendJcrDb.sql
+++ DB2/template/DxV8.5/extendJcrDb.sql
@@ -3,9 +3,40 @@
 # Author: AF - 2016-03-26
 # Owner: Net2Action
 #------------------------------------------------ 
+
+
+PROGNAME=$(basename $0)
+AUTHOR="A.Fontana"
+VERSION="1.0.0"
+OWNER=" by net2action - 2016"
+
+
+function error_exit
+{
+        echo "${PROGNAME}:[$LINENO] ${1}"
+        echo "[$now] ${PROGNAME}:[$LINENO] ${1}" >> ${logFile}
+        msgLine "---------------------------------------"
+        echo "BUILD FAIL"
+        if [ "$DEBUG" == "True" ] ; then
+	    msgLine " DEBUG ------>   Script: $script db: ${DBName} USER ${db2Admin} USING ${db2Pwd}" 1>&2
+	fi
+        exit ${rc}
+}
+
+function msgLine
+{
+        echo "${PROGNAME}:[$LINENO] ${1}" 1>&2
+        echo "[$now] ${PROGNAME}:[$LINENO] ${1}" >> ${logFile}
+}
+
+
+
+
 if [ "$DEBUG" == "True" ] ; then
-   echo " db: ${DBName} USER ${db2Admin} USING ${db2Pwd}"
+   msgLine " DEBUG ------>  db: ${DBName} USER ${db2Admin} USING ${db2Pwd}"
 fi
+
+
 db2 -t -x -z sql.out +p <<-eof
 CONNECT TO ${DBName} USER ${db2Admin} USING ${db2Pwd};
 CREATE BUFFERPOOL ICMLSFREQBP4 SIZE 1000 AUTOMATIC PAGESIZE 4K;
@@ -29,3 +60,17 @@ UPDATE DB CFG FOR ${DBName} USING logbufsz 500;
 UPDATE DB CFG FOR ${DBName} USING DFT_QUERYOPT 2;
 TERMINATE;
 eof
+
+
+
+rc=$?
+if [ ${rc} -ne  0 ] ; then
+        msgLine "Stream sql command fail:  ${rc}"
+        error_exit "$LINENO: Stream sql command [ $script ] fail: ${rc}"
+fi
+
+msgLine "---------------------------------------"
+msgLine "$PROGNAME    BUILD SUCCESS"
+msgLine "End: $(date)"
+
+exit 0
