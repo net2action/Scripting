098fc3330abbf2bbcc20cd85a2113b6ad25bde4b
Andrea Fontana <a.fontana@net2action.com>
2017-01-30 17:02:13 +0100

    preset Hot Backup
    Thsi script collect all db present on Instance and preset a new script to backup all db, if db are configured to support hot backup new script perform a backup online, otherwise try to execute a cald backup of it and treace it on log.

--- DB2/bin/db2CreateList.sh
+++ DB2/bin/db2CreateList.sh
@@ -5,22 +5,24 @@ PROGNAME=$(basename $0)
 AUTHOR="A.Fontana"
 VERSION="1.0.0"
 OWNER="net2action - 2016"
+
 dataHome=/opt/data
 scriptHome=${dataHome}/swScript
 
-mkdir -p ${scriptHome}/log
-log=${scriptHome}/log/$(echo $PROGNAME | cut -d "." -f 1)
-logFile=$log.log
 
-
+if [ -z "$logFile" ]; then
+   mkdir -p ./log
+   log=./log/$(echo $PROGNAME | cut -d "." -f 1)
+   logFile=$log.log
+fi
 
 function error_exit
 {
-	 usage
-
-        echo "${PROGNAME}:[$LINENO] ${1:-"Unknown Error"}"
-          
-        echo "[$now] ${PROGNAME}:[$LINENO] ${1:-"Unknown Error"}" >> ${logFile}
+        usage
+        echo "${PROGNAME}:[$LINENO] ${1}"
+        echo "[$now] ${PROGNAME}:[$LINENO] ${1}" >> ${logFile}
+        msgLine "---------------------------------------"
+        echo "BUILD FAIL"
         exit 1
 }
 
@@ -37,12 +39,13 @@ function usage
   echo "| Author : ${AUTHOR}"
   echo "| Program : ${PROGNAME} ${VERSION}"
   echo "| ========================== "
-  echo "| usge : ${PROGNAME} -i db2Instance"
+  echo "| usge : ${PROGNAME} -i db2Instance -o cmdFile -d debug (True/False)"
   echo "|"
   echo "|"
   echo "*----------------------------------------------------------------------------------"
 }
 
+msgLine "Create backup command for all database $(date)"
 
 # Parse the command-line arguments
 while [ "$#" -gt "0" ]; do
@@ -51,13 +54,21 @@ while [ "$#" -gt "0" ]; do
       db2Instance="$2"
       shift 2
     ;;
+    -d)
+      DEBUG="$2"
+      shift 2
+    ;;
+    -o)
+      cmdFile="$2"
+      shift 2
+    ;;
     -?|--help)
       usage
     exit 0
     ;;
     *)
       break
-    ;;
+    ;; 
   esac
 done
 
@@ -66,25 +77,45 @@ if [ "$db2Instance" == "" ] ; then
 fi
 
 backpath=/backup/${db2Instance}
-
 mkdir -p $backpath
-mkdir -p $scriptHome
-
 msgLine "Your backup will be saved on $backpath"
-
-
 listaDb=`(db2 list database directory | grep alias | cut -f 2 -d"=" | cut -f 2 -d" ")`
-cmdFile=${scriptHome}/bin/db2Backup_${db2Instance}.sh
-msgLine "create self backup script : $cmdFile"
+
 echo "#==================================================================" > $cmdFile
 echo "#  database self backup script       " >> $cmdFile
 echo "#==================================================================" >> $cmdFile
+echo "PROGNAME=\$(basename \$0)"  >> $cmdFile
+echo "AUTHOR=\"A.Fontana\""  >> $cmdFile
+echo "VERSION=\"1.0.0\""  >> $cmdFile
+echo "OWNER=\"net2action - 2016\""  >> $cmdFile
+echo "dataHome=/opt/data"  >> $cmdFile
+echo "scriptHome=\${dataHome}/swScript"  >> $cmdFile
+echo "log=\$scriptHome/log/\$(echo \$PROGNAME | cut -d \".\" -f 1)" >> $cmdFile
+echo "logFile=\$log.log" >> $cmdFile
+
 n=0
 for i in ${listaDb[@]}; do
-    echo $i |
-    echo "echo backup database" $i >> $cmdFile
-    echo 'su - '$Instance'  -c "db2 backup database ' $i ' online to '$backpath' with 2 buffers buffer 1024 parallelism 1 compress include logs without prompting;"'  >> $cmdFile
+    echo "echo backup database $i >> \$logFile" >> $cmdFile
+    cmdStato="db2 GET DB CFG FOR  $i  |  grep LOGARCHMETH1 | cut -d \"=\" -f 2"
+    if [ "$DEBUG" == "True" ]; then
+       msgLine " DEBUG ------> $cmdStato"
+    fi
+    STATO=$(sh -c "db2 GET DB CFG FOR  $i  |  grep LOGARCHMETH1 | cut -d \"=\" -f 2")
+    if [ "$DEBUG" == "True" ]; then
+       msgLine " DEBUG --STATO ----> $i --> $STATO"
+    fi
+
+    if [ "$STATO" = " OFF" ]; then      
+       echo " echo \"This database not configured for hot backup try cold\"  >> \$logFile"  >> $cmdFile
+       echo 'su - '$db2Instance'  -c "db2 backup database ' $i ' to '$backpath' with 2 buffers buffer 1024 parallelism 1 compress without prompting;" >> $logFile'  >> $cmdFile
+    else
+       echo 'su - '$db2Instance'  -c "db2 backup database ' $i ' online to '$backpath' with 2 buffers buffer 1024 parallelism 1 compress include logs without prompting;" >> $logFile'  >> $cmdFile
+    fi
 done
-chmod +x $cmdFile
 msgLine "Your self backup script : $cmdFile was created!"
 
+msgLine "---------------------------------------"
+msgLine "$PROGNAME    BUILD SUCCESS"
+msgLine "End: $(date)"
+
+echo $?
--- DB2/bin/db2FullBackp.sh
+++ DB2/bin/db2FullBackp.sh
@@ -5,22 +5,23 @@ PROGNAME=$(basename $0)
 AUTHOR="A.Fontana"
 VERSION="1.0.0"
 OWNER="net2action - 2016"
+
 dataHome=/opt/data
 scriptHome=${dataHome}/swScript
 
-mkdir -p ../log
-log=../log/$(echo $PROGNAME | cut -d "." -f 1)
-logFile=$log.log
-
-
+if [ -z "$logFile" ]; then
+   mkdir -p ./log
+   log=./log/$(echo $PROGNAME | cut -d "." -f 1)
+   logFile=$log.log
+fi
 
 function error_exit
 {
-	 usage
-
-        echo "${PROGNAME}:[$LINENO] ${1:-"Unknown Error"}"
-          
-        echo "[$now] ${PROGNAME}:[$LINENO] ${1:-"Unknown Error"}" >> ${logFile}
+        usage
+        echo "${PROGNAME}:[$LINENO] ${1}"
+        echo "[$now] ${PROGNAME}:[$LINENO] ${1}" >> ${logFile}
+        msgLine "---------------------------------------"
+        echo "BUILD FAIL"
         exit 1
 }
 
@@ -43,12 +44,12 @@ function usage
   echo "*----------------------------------------------------------------------------------"
 }
 
-msgLine "Start: $(date)"
+msgLine "Database hot backup begin at $(date)"
 
 # Parse the command-line arguments
 while [ "$#" -gt "0" ]; do
   case "$1" in
-    -i)
+    -i) 
       db2Instance="$2"
       shift 2
     ;;
@@ -66,8 +67,24 @@ if [ "$db2Instance" == "" ] ; then
     error_exit "$LINENO: Missing, Db2 Instance name, -i flag is mandatory"
 fi
 
-su - ${db2Instance} -c "${scriptHome}/bin/db2CreateList.sh -i ${db2Instance}"
+mkdir -p $backpath/${db2Instance}
+
+mkdir -p "$scriptHome/bin"
+chown $db2Instance "$backpath/${db2Instance}"
+chown $db2Instance "$scriptHome/bin"
+
+cmdFile=${scriptHome}/bin/db2Backup_${db2Instance}.sh
+
+
+msgLine "create self backup script : $cmdFile"
+
+su - ${db2Instance} -c "${scriptHome}/db2CreateList.sh -i ${db2Instance} -o ${cmdFile} -d $DEBUG"
+msgLine " Execute your first Backup, please wait ...."
 ${scriptHome}/bin/db2Backup_${db2Instance}.sh
 
+
+msgLine "---------------------------------------"
+msgLine "$PROGNAME    BUILD SUCCESS"
 msgLine "End: $(date)"
 
+echo $?
